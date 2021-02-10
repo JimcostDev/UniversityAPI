d2e790f83e54c37631653d45b7e9370d760fb5b2
JimcostDev <rjak727@gmail.com>
2021-02-10 11:14:41 -0500

    Creando el servicio REST con el método HTTP GET y POST para Enrollments

---
+++ University/University.API/Controllers/EnrollmentsController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using University.BL.Data;
+using University.BL.DTOs;
+using University.BL.Models;
+using University.BL.Repositories.Implements;
+using University.BL.Services.Implements;
+
+namespace University.API.Controllers
+{
+    public class EnrollmentsController : ApiController
+    {
+        private IMapper _mapper;
+        private readonly EnrollmentService enrollmentService = new EnrollmentService(new EnrollmentRepository(UniversityContext.Create()));
+        public EnrollmentsController()
+        {
+            //crear mapper
+            _mapper = WebApiApplication.MapperConfiguration.CreateMapper();
+        }
+        #region GET
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()//se devuelve un DTO
+        {
+            var enrollments = await enrollmentService.GetAll();
+            //AutoMapper
+            var enrollmentsDTO = enrollments.Select(x => _mapper.Map<EnrollmentDTO>(x));
+            return Ok(enrollmentsDTO);
+        }
+        #endregion
+
+        #region GET BY ID
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int id)//se devuelve un DTO
+        {
+            var enrollment = await enrollmentService.GetById(id);
+            if (enrollment == null)
+            {
+                return NotFound();
+            }
+
+            var enrollmentDTO = _mapper.Map<EnrollmentDTO>(enrollment);
+
+            return Ok(enrollmentDTO);
+        }
+        #endregion
+
+        #region INSERT
+        /// <summary>
+        /// Crear un objeto de enrollment
+        /// </summary>
+        /// <param name="instruenrollmentDTOctorDTO">Objeto del enrollment</param>
+        /// <returns>Objeto de enrollment</returns>
+        /// <response code="200">Ok. Devuelve el objeto solicitado.</response>
+        /// <response code="400">BadRequest. No se cumple con la validación del modelo.</response>
+        /// <response code="500">InternalServerError. Se ha presentado un error.</response>
+        [HttpPost]
+        public async Task<IHttpActionResult> Insert(EnrollmentDTO enrollmentDTO)//se devuelve un modelo
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var enrollment = _mapper.Map<Enrollment>(enrollmentDTO);
+                enrollment = await enrollmentService.Insert(enrollment);
+                return Ok(enrollment);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+        }
+        #endregion
+
+    }
+}
---
+++ University/University.BL/Repositories/Implements/EnrollmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using University.BL.Data;
+using University.BL.Models;
+
+namespace University.BL.Repositories.Implements
+{
+    public class EnrollmentRepository : GeneryRepository<Enrollment>, IEnrollmentRepository
+    {
+        private readonly UniversityContext _universityContext;
+        public EnrollmentRepository(UniversityContext universityContext) : base(universityContext)
+        {
+            _universityContext = universityContext;
+        }
+        public new async Task<IEnumerable<Enrollment>> GetAll()
+        {
+            var enrollments = _universityContext.Enrollments.Include("Student");
+            return await enrollments.ToListAsync();
+        }
+    }
+}
