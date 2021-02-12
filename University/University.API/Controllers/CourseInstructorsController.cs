736d675a2015d34db38ee1690010506afbfd4ee2
JimcostDev <rjak727@gmail.com>
2021-02-11 21:14:57 -0500

    Creando el servicio REST con el método HTTP PUT Y DELETE para CourseInstructors

--- University/University.API/Controllers/CourseInstructorsController.cs
+++ University/University.API/Controllers/CourseInstructorsController.cs
@@ -78,5 +78,70 @@ namespace University.API.Controllers
         }
         #endregion
 
+        #region PUT
+        /// <summary>
+        /// Modifica un objeto de courseInstructor
+        /// </summary>
+        /// <param name="courseInstructorDTO">Objeto del courseInstructor</param>
+        /// <returns>Objeto de courseInstructor</returns>
+        /// <response code="200">Ok. Devuelve el objeto solicitado.</response>
+        /// <response code="400">BadRequest. No se cumple con la validación del modelo.</response>
+        /// <response code="404">NotFound. No se encuentra el recurso solicitado.</response>
+        /// <response code="500">InternalServerError. Se ha presentado un error.</response>
+        [HttpPut]
+        public async Task<IHttpActionResult> Edit(CourseInstructorDTO courseInstructorDTO, int id)//se devuelve un modelo
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (courseInstructorDTO.ID != id)
+            {
+                return BadRequest();
+            }
+
+            var flag = await courseInstructorService.GetById(id);
+            if (flag == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                var courseInstructor = _mapper.Map<CourseInstructor>(courseInstructorDTO);
+                courseInstructor = await courseInstructorService.Update(courseInstructor);
+                return Ok(courseInstructor);
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+        }
+        #endregion
+
+        #region DELETE
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete(int id)//se devuelve un DTO
+        {
+            var flag = await courseInstructorService.GetById(id);
+            if (flag == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                    await courseInstructorService.Delete(id);
+                    return Ok(); 
+            }
+            catch (Exception ex)
+            {
+
+                return InternalServerError(ex);
+            }
+
+        }
+        #endregion
+
     }
 }
